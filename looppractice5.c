570ede34a1438c0005e4d88e52ddb5e4db57b89b
ShuvoRahmanShuv <shuvorahmanshuv@gmail.com>
2025-04-03 15:49:52 +0600

    modified fact

--- looppractice5.c
+++ looppractice5.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+/*#include <stdio.h>
 int main(){
     int sum = 0;
     for(int i = 1; i <= 10; i++){
@@ -6,4 +6,16 @@ int main(){
     }    
     printf("The sum of the table of 8 is %d\n", sum);
 return 0;
-}
+}*/
+
+
+//factorial of a number:
+ #include <stdio.h>
+int main(){
+    int fact = 1, n = 4;
+    for (int i = 1; i <= n; i++){
+        fact *= i;
+    }
+    printf("The factorial of %d is %d\n", n, fact);
+return 0;
+} 
